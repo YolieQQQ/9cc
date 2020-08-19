697479f636d140bbc09ab18dcb110914f8b0b542
Yolie <yolie@ezff.dev>
2020-08-19 09:18:31 +0900

    エラーメッセージの向上

--- 9cc.c
+++ 9cc.c
@@ -5,28 +5,37 @@
 #include <stdlib.h>
 #include <string.h>
 
-// トークンの種類
 typedef enum {
-    TK_RESERVED,  // 記号
-    TK_NUM,       // 整数トークン
-    TK_EOF,       // 入力の終わりを表すトークン
+    TK_RESERVED, 
+    TK_NUM,       
+    TK_EOF,       
 } TokenKind;
 
 typedef struct Token Token;
 
-// トークン型
 struct Token {
-    TokenKind kind;  // トークンの型
-    Token *next;     // 次の入力トークン
-    int val;         // kindがTK_NUMの場合、その数値
-    char *str;       // トークン文字列
+    TokenKind kind;  
+    Token *next;    
+    int val;         
+    char *str;     
 };
 
-// 現在着目しているトークン
 Token *token;
+char *user_input;
 
-// エラーを報告するための関数
-// printfと同じ引数を取る
+void error_at(char *loc, char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, "");
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
 void error(char *fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
@@ -35,24 +44,18 @@ void error(char *fmt, ...) {
     exit(1);
 }
 
-// 次のトークンが期待している記号のときには、トークンを1つ読み進めて
-// 真を返す。それ以外の場合には偽を返す。
 bool consume(char op) {
     if (token->kind != TK_RESERVED || token->str[0] != op) return false;
     token = token->next;
     return true;
 }
 
-// 次のトークンが期待している記号のときには、トークンを1つ読み進める。
-// それ以外の場合にはエラーを報告する。
 void expect(char op) {
     if (token->kind != TK_RESERVED || token->str[0] != op)
         error("'%c'ではありません", op);
     token = token->next;
 }
 
-// 次のトークンが数値の場合、トークンを1つ読み進めてその数値を返す。
-// それ以外の場合にはエラーを報告する。
 int expect_number() {
     if (token->kind != TK_NUM) error("数ではありません");
     int val = token->val;
@@ -62,7 +65,6 @@ int expect_number() {
 
 bool at_eof() { return token->kind == TK_EOF; }
 
-// 新しいトークンを作成してcurに繋げる
 Token *new_token(TokenKind kind, Token *cur, char *str) {
     Token *tok = calloc(1, sizeof(Token));
     tok->kind = kind;
@@ -71,14 +73,13 @@ Token *new_token(TokenKind kind, Token *cur, char *str) {
     return tok;
 }
 
-// 入力文字列pをトークナイズしてそれを返す
 Token *tokenize(char *p) {
     Token head;
     head.next = NULL;
     Token *cur = &head;
 
     while (*p) {
-        // 空白文字をスキップ
+        
         if (isspace(*p)) {
             p++;
             continue;
@@ -108,20 +109,14 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    // トークナイズする
     token = tokenize(argv[1]);
-
-    // アセンブリの前半部分を出力
+    
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
 
-    // 式の最初は数でなければならないので、それをチェックして
-    // 最初のmov命令を出力
     printf("  mov rax, %d\n", expect_number());
 
-    // `+ <数>`あるいは`- <数>`というトークンの並びを消費しつつ
-    // アセンブリを出力
     while (!at_eof()) {
         if (consume('+')) {
             printf("  add rax, %d\n", expect_number());
